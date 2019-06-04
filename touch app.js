8ca1ac72c5e489e938e39e7c89f61a42dd67e935
Leanna Marshgit config --global user.name Leanna <leanna.marsh@gmail.com>
2019-06-03 20:39:41 -0700

    adding js

---
+++ touch app.js
@@ -0,0 +1,12 @@
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', () => {
+
+}
+
+window.onload = function() {
+  onReady();
+ };
