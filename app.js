cb6e4a1872e7f0763fb237b76aab27c99a8ded84
Leanna Marshgit config --global user.name Leanna <leanna.marsh@gmail.com>
2019-06-06 19:26:23 -0700

    first push

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 function onReady() {
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+       title: newToDoText.value,
+       complete: false
+     });
+     newToDoText.value = '';
+
+     renderTheUI();
+
+  });
+}
+
+function renderTheUI() {
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
+  toDoList.textContent = '';
 
-    // get the text
-    let title = newToDoText.value;
-
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
+  toDos.forEach(function(toDo) {
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
     checkbox.type = "checkbox";
-    // attach a delete button
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
 
-    deleteBtn.addEventListener("click", function(event){
-      toDoList.removeChild(this.parentElement);    
-    })
+    newLi.textContent = toDo.title;
 
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the delete button to the li
-    newLi.appendChild(deleteBtn);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
-    newToDoText.value = '';
+          toDoList.appendChild(newLi);
+          newLi.appendChild(checkbox);
   });
+
+}
+
+addToDoForm.addEventListener('submit', event => {
+  event.preventDefault();
+  createNewToDo();
+});
+
+renderTheUI();
 }
 
 window.onload = function() {
